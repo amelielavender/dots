cb12ec5f07f3cc13dcca79dc4cc77df7b90e4187
amelie lavender <lavenderamelie@gmail.com>
2023-03-08 03:04:07 -0500

    fix capitalization

--- scripts/wacom.sh
+++ scripts/wacom.sh
@@ -3,27 +3,27 @@
 #see https://github.com/linuxwacom/xf86-input-wacom/wiki/xorg.conf.d for a hotplug configuration
 
 #get name of devices from `xsetwacom list`
-TAB="Wacom Cintiq Pro 13"
+tab="Wacom Cintiq Pro 13"
 
-PEN="$TAB Pen stylus"
-PAD="$TAB Pad pad"
-ERASER="$TAB Pen eraser"
-TOUCH="$TAB Touch Finger touch"
+pen="$tab Pen stylus"
+pad="$tab Pad pad"
+eraser="$tab Pen eraser"
+touch="$tab Touch Finger touch"
 
 #for nvidia gpus, need to use HEAD-0, HEAD-1 as display names instead of the name under xrandr. just headass tbh
-MONITOR="HEAD-1"
+monitor="HEAD-1"
 
 #pen
-xsetwacom set "$PEN" MapToOutput $MONITOR 
-xsetwacom set "$PEN" Button 2 3
-xsetwacom set "$PEN" Button 3 2
+xsetwacom set "$pen" MapToOutput $monitor 
+xsetwacom set "$pen" Button 2 3
+xsetwacom set "$pen" Button 3 2
 
 #eraser
-xsetwacom set "$ERASER" MapToOutput $MONITOR
-xsetwacom set "$ERASER" Button 2 3
-xsetwacom set "$ERASER" Button 3 2
+xsetwacom set "$eraser" MapToOutput $monitor
+xsetwacom set "$eraser" Button 2 3
+xsetwacom set "$eraser" Button 3 2
 
 #touch
-xsetwacom set "$TOUCH" touch off
+xsetwacom set "$touch" touch off
 
 echo "settings applied"
