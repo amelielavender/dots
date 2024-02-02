c80fff032e0668f6d61ebfdc54125e92fcc05f6c
amelie lavender <lavenderamelie@gmail.com>
2024-02-02 02:33:27 -0500

    add random lsp to try them out finally

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,15 +5,23 @@
 require('user.settings')
 require('user.plugins')
 require('user.highlights')
-require('lspconfig').pyright.setup{}
-require('lspconfig').html.setup{}
-require('lspconfig').lua_ls.setup{}
-require('lspconfig').ccls.setup{}
-require('lspconfig').sqlls.setup{}
-require'lspconfig'.tsserver.setup{}
-require'lspconfig'.tailwindcss.setup{}
-require'lspconfig'.emmet_language_server.setup{
-  init_options = {
-    showSuggestionsAsSnippets = false,
-  },
+
+local coq = require'coq'
+local lsp = require'lspconfig'
+
+-- language servers 
+
+lsp.pyright.setup{}
+lsp.html.setup{
+  coq.lsp_ensure_capabilities()
+}
+lsp.lua_ls.setup{
+  coq.lsp_ensure_capabilities()
+}
+lsp.ccls.setup{}
+lsp.sqlls.setup{}
+lsp.tsserver.setup{}
+lsp.tailwindcss.setup{}
+lsp.emmet_language_server.setup{
+  coq.lsp_ensure_capabilities()
 }
